0513be19aa9218bfa3fd09c94646016e1fcb5354
AVossos <97504769+AVossos@users.noreply.github.com>
2022-07-12 20:42:21 +0300

    Update settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -16,7 +16,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #MEDA_ROOT = Path.join()
 
 # Quick-start development settings - unsuitable for production
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = "/static/"
+
+
